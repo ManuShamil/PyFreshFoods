f1a5edbb59e29bd7041595cdb29b5e6fbfcbbd7f
Manu Shamil <manusshamil@gmail.com>
2020-04-03 14:08:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ class Main:
 
         myUser:User = UserLogin('user8@gmail.com').loginEmail('user123')
 
-        myUser.placeOrder(item, 11)
-
+        myUser.placeOrder(item, 8)
+        myUser.placeOrder(item, 8)
         #userDetails = UserDetails('FreshFoods','User 2','2000','Kerala, India')
 
         #manageUser = UserManagement(myUser, userDetails)
